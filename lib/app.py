b3419b638797a5419988737ded8513e65df7b802
Emilymatoke <emilymomata@gmail.com>
2024-02-29 15:02:04 +0300

    Running python code

--- lib/app.py
+++ lib/app.py
@@ -1,4 +1,7 @@
 #lib/app.py (comment)
 print("Hello World!") #prints output
 
-#To run python it is done in the terminal using the 'python/filename.py'
+#To run python it is done in the terminal using the 'python filename.py' i.e python3 app.py for this case
+
+print ("My name is Emily.")
+print("I love the sun!")
