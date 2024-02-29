f18e39dd823c1e0fed78f794fab86d6d54dbc05b
Emilymatoke <emilymomata@gmail.com>
2024-02-29 15:50:43 +0300

    Pytest

--- lib/app.py
+++ lib/app.py
@@ -1,13 +1,15 @@
 #lib/app.py (comment)
-print("Hello World!") #prints output
+#print("Hello World!") #prints output
 
 #To run python it is done in the terminal using the 'python filename.py' i.e python3 app.py for this case
-
-print ("My name is Emily.")
-print("I love the sun!")
-
 #For paragraphs:
+#There is a python shell in the terminal ; just type Python
+#You can print there. Also when assigning name we use snake casa i,.e separate with underscores.
+#Get out of the shell using ctrl+d
+#To install pytest : install pipenv using sudo apt install pipenv ; To create a virtual environment use 
+
+
 
 print("Hello World!", end=" ")
-print("My name is Emily.", end="!! ")
-print("I love the sun!", end="!!!/n")
+print("I am Emily!", end="!! ")
+print("I love the sun!", end="!!!/n ")
