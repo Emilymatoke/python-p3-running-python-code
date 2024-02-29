275052b612175aa01e1fd67a6b77fa6cdb7dc636
Emilymatoke <emilymomata@gmail.com>
2024-02-29 16:00:02 +0300

    Completed

--- lib/app.py
+++ lib/app.py
@@ -7,9 +7,7 @@
 #You can print there. Also when assigning name we use snake casa i,.e separate with underscores.
 #Get out of the shell using ctrl+d
 #To install pytest : install pipenv using sudo apt install pipenv ; To create a virtual environment use 
-
-
+#for pytest; sudo apt install pipenv; pipenv  --version 3.12; pipenv shell
 
 print("Hello World!", end=" ")
-print("I am Emily!", end="!! ")
-print("I love the sun!", end="!!!/n ")
+print("Pass this test, please.") 
